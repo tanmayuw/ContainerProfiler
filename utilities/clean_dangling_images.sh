98ff657393c286d05658b0139f2f457651d36e30
Tanmay <tanmay@uw.edu>
2021-08-23 23:33:15 -0700

    configs fixed

--- utilities/clean_dangling_images.sh
+++ utilities/clean_dangling_images.sh
@@ -2,12 +2,13 @@
 echo "Found the following dangling images: "
 docker images --filter "dangling=true" -q --no-trunc
 
-echo "remove all to free space?(y/N)"
-read response
-
-if [ "$response" = "${answer#[Yy]}" ] ;
+read -p "Do you with to remove these images to free space? " -n 1 -r
+echo    # (optional) move to a new line
+if [[ $REPLY =~ ^[Yy]$ ]]
 then
 	docker rmi $(docker images --filter "dangling=true" -q --no-trunc)
 else
 	echo "exiting... "
 fi
+
+
